257a891240d7ac4a046bf0b1033c0be7de8cd08c
Pavel <chago81@inbox.ru>
2020-08-10 12:09:53 +0300

    Добавление картинок и других ресурсов

--- StudentSim/Game.cs
+++ StudentSim/Game.cs
@@ -13,7 +13,7 @@ namespace StudentSim
 {
     public partial class GForm : Form
     {
-        private City city;
+        private City city; //Проба гита.
         public static Student student;
         public GForm()
         {
